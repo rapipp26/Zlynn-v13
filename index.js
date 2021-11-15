de1146b86063eaeb7f3bfdfc65f4cdbf8cd1e0c0
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-11-15 10:57:48 +0700

    Commit: #39

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const { glob } = require("glob");
 const PG = promisify(glob);
 const Ascii = require("ascii-table");
 
-client.commands = new Collection()
+client.commands = new Collection();
+
+const { DisTube } = require("distube");
+const { SpotifyPlugin } = require("@distube/spotify");
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: true,
+    leaveOnFinish: true,
+    emitAddSongWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
+});
+module.exports = client;
 
 require("./mongo.js")(client);
 require("./Systems/GiveawaySys")(client);
