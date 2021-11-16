655a84172f4a845ca29b959a1739fa3fdfdfb3de
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-11-16 13:06:32 +0700

    Commit: #59

--- Events/Distube/DistubeEvents.js
+++ Events/Distube/DistubeEvents.js
@@ -20,7 +20,7 @@ client.distube
 
     .on("addList", (queue, playlist) => queue.textChannel.send({ embeds: [new MessageEmbed()
     .setAuthor("Added Playlist")
-    .setDescription(`${true1} **|* Added \`${playlist.name}\`\n${setting} **|** To the queue\n${status(queue)}`)
+    .setDescription(`${true1} **|** Added \`${playlist.name}\`\n${setting} **|** To the queue\n${status(queue)}`)
     .setColor("BLURPLE")
     .setFooter("➕")
     .setTimestamp()]}))
