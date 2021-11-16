3bcc53c743d02425a319a2ae9827c9021d6ec57f
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-11-16 10:50:44 +0700

    Commit: #56

--- Events/Distube/DistubeEvents.js
+++ Events/Distube/DistubeEvents.js
@@ -1,26 +1,26 @@
 const client = require("../../index");
 const { MessageEmbed } = require("discord.js");
-const { true1, false1, arrow, reply1, reply2, thumbsup, thumbsdown } = require ('../../config.json');
+const { true1, false1, arrow, reply1, reply2, thumbsup, thumbsdown, human, piano, setting } = require ('../../config.json');
 
 const status = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(", ") || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
 client.distube
     .on("playSong", (queue, song) => queue.textChannel.send({ embeds: [new MessageEmbed()
     .setAuthor("Playing a Music.")
-    .setDescription(`${true1} **|** Playing \`${song.name}\` - \`${song.formattedDuration}\`\n${status(queue)}\nRequested by: ${song.user}`)
+    .setDescription(`${piano} **|** Playing \`${song.name}\` - \`${song.formattedDuration}\`\n${setting} **|** ${status(queue)}\n${human} **|** Requested by: ${song.user}`)
     .setColor("BLURPLE")
     .setFooter("▶")
     .setTimestamp() ] }))
 
     .on("addSong", (queue, song) => queue.textChannel.send({ embeds: [new MessageEmbed()
     .setAuthor("Added new song to the queue.")
-    .setDescription(`${true1} **|** Added \`${song.name}\` - \`${song.formattedDuration}\`\nTo the queue by ${song.user}`)
+    .setDescription(`${piano} **|** Added \`${song.name}\` - \`${song.formattedDuration}\`\n${human} **|** To the queue by ${song.user}`)
     .setColor("BLURPLE")
     .setFooter("➕")
     .setTimestamp() ] }))
 
     .on("addList", (queue, playlist) => queue.textChannel.send({ embeds: [new MessageEmbed()
     .setAuthor("Added Playlist")
-    .setDescription(`${true1} **|** Added \`${playlist.name}\` to the queue\n${status(queue)}`)
+    .setDescription(`${piano} **|* Added \`${playlist.name}\`\n${setting} **|** To the queue\n${status(queue)}`)
     .setColor("BLURPLE")
     .setFooter("➕")
     .setTimestamp()]}))
