725c6c0cfd2afbeba000d3aa139ea67248796ea2
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-11-21 10:42:24 +0700

    Commit: #83

--- Events/Message/messageCreate.js
+++ Events/Message/messageCreate.js
@@ -1,8 +1,7 @@
 module.exports = {
     name: "messageCreate",
     execute(client, message) {
-        if (message.mentions.has(client.user)) { 
-            message.channel.send('All command is using slash command.')
-             }
+        if (message.mentions.has(client.user)) return message.channel.send('All command is using slash command.')
+             
     }
 }
