a3e9271f12b096f1356ff8932c2f0df307a2414d
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-11-30 14:07:16 +0700

    Commit: #165

--- Commands/Info/covid.js
+++ Commands/Info/covid.js
@@ -4,14 +4,14 @@ const config = require ('../../Structures/config.json');
 module.exports = { 
     name: 'covid', 
     description: 'Information about covid in worldwide or specified country.',
-    options: [
-        {
-            name: "country",
-            description: "Provide a country to check",
-            type: "STRING",
-            required: false
-        }
-    ],
+    //ptions: [
+    //   {
+    //         name: "country",
+    //        description: "Provide a country to check",
+    //        type: "STRING",
+    //        required: false
+    //    }
+    //],
     /**
      * 
      * @param {CommandInteraction} interaction 
@@ -19,38 +19,56 @@ module.exports = {
      */
     async execute(interaction, client) {
         const axios = require('axios');
-        const c = await interaction.options.getString("country") || "";
+        //const c = await interaction.options.getString("country") || "";
         const embed = new MessageEmbed()
 
         try {
             const response2 = await axios.get(`https://disease.sh/v3/covid-19/all`);
             console.log(response2.data.cases)
 
-            embed.setAuthor("Covid-19 Information ^0^", client.user.avatarURL({ format: "png" }))
+            embed.setAuthor("Covid Cases Worldwide 🦠", client.user.avatarURL({ format: "png" }))
             .setColor("DARK_BLUE")
-            .setDescription(
-               `\`\`\`\css`
-                +
-                "Covid Cases Worldwide 🦠"
-                +
-                "\n\n"
-                +
-                `[ Total Cases ] : ${response2.data.cases}`
-                `[ Today Cases ] : ${response2.data.todayCases}`
-                +
-                "\n"
-                +
-                `[ Total Deaths ] : ${response2.data.deaths}`
-                `[ Today Deaths] : ${response2.data.todayDeaths}`
-                +
-                `[ Total Recovered ] : ${response2.data.recovered}`
-                `[ Today Recovered] : ${response2.data.todayRecovered}`
-                +
-                `[ Active ] : ${response2.data.active}`
-                `[ Critical] : ${response2.data.critical}`
-                +
-                `[ World Population ] : ${response2.data.population}`
-                `\`\`\``
+            .addFields(
+                {
+                    name: "Total Cases",
+                    value: `${response2.data.cases}`,
+                    inline: true
+                },
+                {
+                    name: "Today Cases",
+                    value: `${response2.data.todayCases}`,
+                    inline: true
+                },
+                {
+                    name: "Total Death",
+                    value: `${response2.data.deaths}`,
+                    inline: true
+                },
+                {
+                    name: "Today Deaths",
+                    value: `${response2.data.todayDeaths}`,
+                    inline: true
+                },
+                {
+                    name: "Active",
+                    value: `${response2.data.active}`,
+                    inline: true
+                },
+                {
+                    name: "Critical",
+                    value: `${response2.data.critical}`,
+                    inline: true
+                },
+                {
+                    name: "World Population",
+                    value: `${response2.data.population}`,
+                    inline: true
+                },
+                {
+                    name: "Last Updated",
+                    value: `<t:${parseInt(response2.data.updated)}:F>`,
+                    inline: true
+                },
             )
             .addField("Last Updated", `<t:${parseInt(response2.data.updated)}:F>`)
             .setFooter(`Executed by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
