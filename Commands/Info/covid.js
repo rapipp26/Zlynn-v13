dc6513bc20f2b3a47da901f9916821d42a4f2ee0
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-12-01 07:47:27 +0700

    Commit: #173

--- Commands/Info/covid.js
+++ Commands/Info/covid.js
@@ -1,17 +1,22 @@
-const { CommandInteraction, MessageEmbed, Client } = require('discord.js');
+const { CommandInteraction, MessageEmbed, Client, MessageAttachment } = require('discord.js');
 const config = require ('../../Structures/config.json');
 
 module.exports = { 
     name: 'covid', 
     description: 'Information about covid in worldwide or specified country.',
-    //ptions: [
-    //   {
-    //         name: "country",
-    //        description: "Provide a country to check",
-    //        type: "STRING",
-    //        required: false
-    //    }
-    //],
+    options: [
+        {
+            name: "all",
+            description: "Shows covid-19 status worldwide",
+            type: "SUB_COMMAND"
+        },
+        {
+            name: "country",
+            description: "Shows covid-19 at the procided country",
+            type: "SUB_COMMAND",
+            options:[{ name: "country_name", description: "Provide the country", type: "STRING", required: true }]
+        },
+    ],
     /**
      * 
      * @param {CommandInteraction} interaction 
@@ -19,54 +24,109 @@ module.exports = {
      */
     async execute(interaction, client) {
         const axios = require('axios');
-        //const c = await interaction.options.getString("country") || "";
         const embed = new MessageEmbed()
+        .setFooter(`Executed by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp();
+        const subc = interaction.options.getSubcommand();
 
         try {
-            const response2 = await axios.get(`https://disease.sh/v3/covid-19/all`);
-            console.log(response2.data.cases)
+            switch(subc) {
+                case "all" : {
+                    const response2 = await axios.get(`https://disease.sh/v3/covid-19/all`);
+        
+                    embed.setAuthor("Covid Cases Worldwide 🦠", client.user.avatarURL({ format: "png" }))
+                    .setColor("DARK_BLUE")
+                    .setThumbnail("https://c.tenor.com/Aqe8V3wni4QAAAAC/covid19-coronavirus.gif")
+                    .addFields(
+                        {
+                            name: "Total Cases",
+                            value: `\`\`\`${response2.data.cases.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Today Cases",
+                            value: `\`\`\`${response2.data.todayCases.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Total Death",
+                            value: `\`\`\`${response2.data.deaths.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Today Deaths",
+                            value: `\`\`\`${response2.data.todayDeaths.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Active",
+                            value: `\`\`\`${response2.data.active.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Critical",
+                            value: `\`\`\`${response2.data.critical.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "World Population",
+                            value: `\`\`\`${response2.data.population.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Last Updated",
+                            value: `<t:${parseInt(response2.data.updated / 1000 )}:F>`
+                        },
+                    )
+        
+                    interaction.reply({ embeds: [embed] })
+                }
+                case "country" : {
+                    const cnt = await interaction.options.getString("country_name")
+                    const resp = await axios.get(`https://disease.sh/v3/covid-19/countries/${cnt}`)
 
-            embed.setAuthor("Covid Cases Worldwide 🦠", client.user.avatarURL({ format: "png" }))
-            .setColor("DARK_BLUE")
-            .setThumbnail("https://c.tenor.com/Aqe8V3wni4QAAAAC/covid19-coronavirus.gif")
-            .addFields(
-                {
-                    name: "Total Cases",
-                    value: `\`\`\`${response2.data.cases.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Today Cases",
-                    value: `\`\`\`${response2.data.todayCases.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Total Death",
-                    value: `\`\`\`${response2.data.deaths.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Today Deaths",
-                    value: `\`\`\`${response2.data.todayDeaths.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Active",
-                    value: `\`\`\`${response2.data.active.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Critical",
-                    value: `\`\`\`${response2.data.critical.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "World Population",
-                    value: `\`\`\`${response2.data.population.toLocaleString()}\`\`\``
-                },
-                {
-                    name: "Last Updated",
-                    value: `<t:${parseInt(response2.data.updated / 1000 )}:F>`
-                },
-            )
-            .setFooter(`Executed by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
-            .setTimestamp();
+                    embed.setAuthor(`${resp.data.country} Covid-19 Information 🦠`)
+                    .setColor("DARK_RED")
+                    .addFields(
+                        {
+                            name: "Country Iso",
+                            value: `${resp.data.countryInfo.iso3}`,
+                            inline: true
+                        },
+                        {
+                            name: "Country Continent",
+                            value: `${resp.data.continent}`,
+                            inline: true
+                        },
+                        {
+                            name: "Total Cases",
+                            value: `\`\`\`${resp.data.cases.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Today Cases",
+                            value: `\`\`\`${resp.data.todayCases.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Total Death",
+                            value: `\`\`\`${resp.data.deaths.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Today Deaths",
+                            value: `\`\`\`${resp.data.todayDeaths.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Active",
+                            value: `\`\`\`${resp.data.active.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Critical",
+                            value: `\`\`\`${resp.data.critical.toLocaleString()}\`\`\``
+                        },
+                        {
+                            name: "Last Updated",
+                            value: `<t:${parseInt(resp.data.updated / 1000 )}:F>`
+                        },
+                    );
+                    const flag = await axios.get(`${resp.data.flag}`)
+                    const att = new MessageAttachment(flag.data.link, "country.png")
+                    interaction.reply({ embeds: [embed], files: [att] })
 
-            interaction.reply({ embeds: [embed] })
+                    
+                }
+            }
     } catch (error) {
                 embed.setTitle("⚠ An error occurred ⚠")
                 .setColor("YELLOW")
