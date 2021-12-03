0ae62ba19281bd74978ffc9aa0c4ee2454e6d884
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-12-03 19:29:44 +0700

    Commit: #189

--- Commands/Info/status.js
+++ Commands/Info/status.js
@@ -1,62 +1,47 @@
-const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const { CommandInteraction, Client, MessageEmbed, Guild } = require("discord.js");
 const { connection } = require("mongoose");
-require("../../Events/Client/ready.js")
-const { reply1, reply2 } = require("../../Structures/config.json")
-
+const config = require("../../Structures/config.json")
 
 module.exports = {
-    name: "status",
-    description: "Gives the bot database, ping, uptime status",
+    name: "botinfo",
+    description: "Shows the bot's status",
     /**
      * 
      * @param {CommandInteraction} interaction 
      * @param {Client} client 
      */
     async execute(interaction, client) {
-
         const Response = new MessageEmbed()
-        .setAuthor("My status and information!")
-        .setColor("PURPLE")
-        .addFields(
-            {
-                name: "🤖・Client",
-                value: `${reply1} \`🟢 ONLINE\``,
-                inline: true
-            },
-            {
-                name: "🏓・Ping",
-                value: `${reply1} \`${client.ws.ping}ms\``,
-                inline: true
-            },
-            {
-                name: "⏲・Uptime",
-                value: `${reply1} <t:${parseInt(client.readyTimestamp / 1000)}:R>`,
-                inline: true
-            },
-            {
-                name: "🏬・Database",
-                value: `${reply1} \`${switchTo(connection.readyState)}\``,
-                inline: true
-            },
-        )
-        .setFooter(`Executed by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
-        .setTimestamp();
-        interaction.reply({ embeds: [Response] })
+            .setColor("BLURPLE")
+            .setAuthor("My information! >\"< ")
+            .setDescription(`**Client**: \`🟢 ONLINE\` - \`${client.ws.ping}ms\`\n **Uptime**: <t:${parseInt(client.readyTimestamp / 1000)}:R>\n
+            **Database**: \`${switchTo(connection.readyState)}\` \n \n **Program**: \n🔋・**Node.js**: \`${process.version}\`\n${config.logo}・**Discord.js**: \`${require("discord.js").version}\`\n🧰・**MongoDB**: \`${require("mongoose").version}\`\n⏱・**Mongoose**: \`${require("mongoose").version}`)
+            .addField("**__Commands__**", `\`${client.commands.size}\` commands loaded.`, true)
+            .addField("**__Guilds__**", `\`${client.guilds.cache.size}\` guilds connected.`, true)
+            .addField("**__Users__**", `\`${client.users.cache.size}\` users connected.`, true)
+            .setThumbnail(client.user.avatarURL({ format: "png", dynamic: true, size: 1024 }))
+            .setFooter(`Executed by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
+            .setTimestamp();
 
+        interaction.reply({ embeds: [Response], ephemeral: true });
     }
-}
+};
 
 function switchTo(val) {
     var status = " ";
-    switch(val) {
-        case 0 : status = `🔴 DISCONNECTED`
-        break;
-        case 1 : status = `🟢 CONNECTED`
-        break;
-        case 2 : status = `🟠 CONNECTING`
-        break; 
-        case 3 : satus = `🟣 DISCONNECTING`
-        break;
+    switch (val) {
+        case 0:
+            status = "🔴 DISCONNECTED";
+            break;
+        case 1:
+            status = `🟢 CONNECTED`
+            break;
+        case 2:
+            status = `🟡 CONNECTING`
+            break;
+        case 3:
+            status = `🔵 DISCONNECTING`
+            break;
     }
-    return status; 
+    return status;
 }
