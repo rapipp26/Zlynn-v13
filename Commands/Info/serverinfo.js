497ab2ad1859eaac2d264fc7c1322a121d81bafb
NotFlynn <78672226+NotFlynn@users.noreply.github.com>
2021-12-15 17:26:30 +0700

    Commit: #202

--- Commands/Info/serverinfo.js
+++ Commands/Info/serverinfo.js
@@ -23,27 +23,55 @@ module.exports = {
                 name: "General Information",
                 value: 
                 `
-                ╭ Name : ${guild.name}\n︱Created : <t:${parseInt(guild.createdTimestamp / 1000)}:f>\n︱Owner : <@${guild.ownerId}>\n╰ Description : ${guild.description}
+                ・Name : ${guild.name}
+                ・Created : <t:${parseInt(guild.createdTimestamp / 1000)}:R>
+                ・Owner : <@${guild.ownerId}>
+                ・Description : ${guild.description || "No description."}
+                ・Roles : ${guild.roles.cache.size}
                 `
             },
             {
                 name: "Users Information",
                 value:
                 `
-                ╭ Member : ${guild.members.cache.filter((m) => !m.user.bot).size }\n︱Bots : ${guild.members.cache.filter((m) => m.user.bot).size}\n╰ Total : ${guild.memberCount}
+                ・Member : ${guild.members.cache.filter((m) => !m.user.bot).size }
+                ・Bots : ${guild.members.cache.filter((m) => m.user.bot).size}
+
+                ・Users Total : ${guild.memberCount}
                 `
             },
             {
                 name: "Channels Information",
                 value:
                 `
-                ╭ Text : ${guild.channels.cache.filter((c) => c.type === "GUILD_TEXT").size}
-                ︱Voice : ${guild.channels.cache.filter((c) => c.type === "GUILD_VOICE").size}
-                ︱Threads : ${guild.channels.cache.filter((c) => c.type === "GUILD_NEWS_THREAD" && "GUILD_PUBLIC_THREAD" && "GUILD_PRIVATE_THREAD").size}
-                ︱Category : ${guild.channels.cache.filter((c) => c.type === "GUILD_CATEGORY").size}
-                ︱News: ${guild.channels.cache.filter((c) => c.type === "GUILD_NEWS").size}
-                ︱Stage : ${guild.channels.cache.filter((c) => c.type === "GUILD_STAGE_VOICE").size}
-                ╰ Total: ${guild.channels.cache.size}
+                ・Text : ${guild.channels.cache.filter((c) => c.type === "GUILD_TEXT").size}
+                ・Voice : ${guild.channels.cache.filter((c) => c.type === "GUILD_VOICE").size}
+                ・Threads : ${guild.channels.cache.filter((c) => c.type === "GUILD_NEWS_THREAD" && "GUILD_PUBLIC_THREAD" && "GUILD_PRIVATE_THREAD").size}
+                ・Category : ${guild.channels.cache.filter((c) => c.type === "GUILD_CATEGORY").size}
+                ・News : ${guild.channels.cache.filter((c) => c.type === "GUILD_NEWS").size}
+                ・Stage : ${guild.channels.cache.filter((c) => c.type === "GUILD_STAGE_VOICE").size}
+
+                ・Channels Total : ${guild.channels.cache.size}
+                `
+            },
+            {
+                name: "Emojis & Stickers Information",
+                value: 
+                `
+                ・Animated : ${guild.emojis.cache.filter((e) => e.animated).size}
+                ・Static : ${guild.emojis.cache.filter((e) => !e.animated).size}
+                ・Stickers : ${guild.stickers.cache.size}
+
+                ・Emojis & Stickers Total : ${stickers.cache.size + emojis.cache.size}
+                `
+            },
+            {
+                name: "Nitro Statistic",
+                value: 
+                `
+                ・Boosts : ${guild.premiumSubscriptionCount}
+                ・Boosters : ${guild.members.cache.filter((m) => m.premiumSince).size}
+                ・Boost Tier : ${guild.premiumTier.replace("TIER_","")}
                 `
             }
         )
