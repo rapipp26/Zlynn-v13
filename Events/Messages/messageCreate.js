201b9938d4c797b7db9f56350a5e6ecf6da7682e
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-01-09 15:28:12 +0700

    Commit: #216

--- Events/Messages/messageCreate.js
+++ Events/Messages/messageCreate.js
@@ -5,9 +5,9 @@ const GuildSchema = require("../../Schemas/blacklistwordDB")
 module.exports = {
 name: "messageCreate",
 async execute(message, client) {
+  if (message.author.bot) return false;
 
-
-  if(message.includes(client.user)) return message.reply({ content: "Hi!, this bot only support \`Slash Commands\` use \`/help\` to get the commands list"})
+  if(message.mentions.includes(client.user)) return message.reply({ content: "Hi!, this bot only support \`Slash Commands\` use \`/help\` to get the commands list"})
 
         let GuildData;
       try {
