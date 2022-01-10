f19f3469a20e08278a92e8578115c3ceff62e8cf
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-01-10 18:10:33 +0700

    Commit: #222

---
+++ Events/Guild/guildCreate.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "guildCreate",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(guild) {
+        process.exit();
+    }
+}
