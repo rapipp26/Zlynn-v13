903b0df01f0a5c2656233549cfa3667bfddb893f
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-01 14:48:17 +0700

    Commit: #301

--- Commands/Moderation/moderate.js
+++ Commands/Moderation/moderate.js
@@ -33,7 +33,7 @@ module.exports = {
     async execute(interaction, client) {
         const target = interaction.options.getMember("member");
         const reason = interaction.options.getString("reason") || "No reason provided.";
-        const time = ms(interaction.options.getString("time")) || 0;
+        const time = ms(interaction.options.getString("time") || 0)
 
         if(target.roles.highest.position >= interaction.member.roles.highest.position) return interaction.reply({ content: `${client.config.cancel} You can't moderate this member, it has the same/higher role position than you`});
         if(target.user.id.includes(interaction.guild.ownerId)) return interaction.reply({ content: `${client.config.cancel} You can't moderate the server owner, me neither can't`});
