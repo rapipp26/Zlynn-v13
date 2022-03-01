724b7b49fa1d6efe8a855ea4301b78c3384a6932
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-01 14:13:50 +0700

    Commit: #295

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -3,8 +3,9 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Successfully connected to ${client.user.tag}!`)
-        client.user.setActivity('Please mention me for more information!', { type: 'PLAYING' });
-        client.user.setStatus('idle');
+        await client.user.setActivity('Mention me for more information! 🐸', { type: 'PLAYING' });
+        await client.user.setStatus('idle');
+        await client.channels.cache.get("948060231156498466").send({ content: "Im ready."})
 
         require("../../Systems/chatFilter");
     }
