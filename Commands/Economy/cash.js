bc80d8d64365319c897b2d201cfe618b82e58a2e
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-02 15:46:43 +0700

    Commit: #302

---
+++ Commands/Economy/cash.js
@@ -0,0 +1,102 @@
+const { CommandInteraction, MessageEmbed, Client, Message, MessageActionRow, MessageButton } = require('discord.js');
+const schema = require("../../Schemas/economyDB");
+const ms = require("parse-ms");
+
+module.exports = {
+    name: "cash",
+    cooldown: 5,
+    description: "A simple economy command.",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+
+        schema.findOne({ userId : target.id }, async(err, docs) => {
+            if(err) throw err;
+            if(!docs) docs = await schema.create({ userId: target.id });
+
+            const embed = new MessageEmbed()
+            .setAuthor({ name: `${interaction.user.tag}'s Cash Info 💳`}, client.user.displayAvatarURL({ format: "png" }))
+            .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
+            .setColor("RANDOM");
+
+            const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setLabel("Deposit")
+                .setStyle("SUCCESS")
+                .setCustomId("depo")
+                .setEmoji(`${client.config.bank}`),
+                new MessageButton()
+                .setLabel("Withdraw")
+                .setStyle("SUCCESS")
+                .setCustomId("with")
+                .setEmoji(`${client.config.dollar}`),
+                new MessageButton()
+                .setLabel("Daily")
+                .setStyle("SUCCESS")
+                .setCustomId("dai")
+                .setEmoji(`${client.config.daily}`),
+            )
+
+            const row2 = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setLabel("Deposit")
+                .setStyle("SUCCESS")
+                .setCustomId("depo")
+                .setEmoji(`${client.config.bank}`),
+                new MessageButton()
+                .setLabel("Withdraw")
+                .setStyle("SUCCESS")
+                .setCustomId("with")
+                .setEmoji(`${client.config.dollar}`),
+                new MessageButton()
+                .setLabel("Daily")
+                .setStyle("DANGER")
+                .setCustomId("dai")
+                .setEmoji(`${client.config.daily}`),
+            )
+
+            let timeout = 86400000
+            let ra = Math.floor(Math.random() * 2000 ) + 500
+
+            //Daily button
+            if(timeout - (Date.now() - docs.daily) > 0) {
+                let tl = ms(timeout - (Date.now() - docs.daily))
+                await embed.addFields(
+                    {
+                        name: `${client.config.dollar} Cash`,
+                        value: `\`\`\`js\n${docs.cash.toLocaleString()}\n\`\`\``
+                    },
+                    {
+                        name: `${client.config.bank} PiggyBank`,
+                        vaue: `\`\`\`js\n${docs.bank.toLocaleString()}\n\`\`\``
+                    },
+                    {
+                        name: `${client.config.daily} Daily Cash`,
+                        value: `**${tl.hours}h, ${tl.minutes}m, ${tl.seconds}s**`
+                    }
+                );
+                return interaction.reply({ embeds: [embed], components: [row2] })
+            } else {
+                await embed.addFields(
+                    {
+                        name: `${client.config.dollar} Cash`,
+                        value: `\`\`\`js\n${docs.cash.toLocaleString()}\n\`\`\``
+                    },
+                    {
+                        name: `${client.config.bank} PiggyBank`,
+                        vaue: `\`\`\`js\n${docs.bank.toLocaleString()}\n\`\`\``
+                    },
+                )
+                .setDescription(`${client.config.checked} Your daily cash is ready! Claim it by clicking the button below.`)
+                return interaction.reply({ embeds: [embed], components: [row] })
+            }
+            //Daily button
+
+    })
+}
+}
