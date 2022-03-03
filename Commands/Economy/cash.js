69817610625385fd788006b6f6a4db3aa4660fb5
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-03 07:12:55 +0700

    Commit: #326

--- Commands/Economy/cash.js
+++ Commands/Economy/cash.js
@@ -163,13 +163,13 @@ module.exports = {
                     case "depo" :
                         const fil = msg => msg.author.id === user.id;
                         await i.reply({ content: "Please type the amount that you want to deposit.", fetchReply: true }).then(() => {
-                            i.channel.awaitMessages({ fil, max: 3, time: 30000, errors: ["time"] }).then(async col=> {
+                            i.channel.awaitMessages({ fil, max: 3, time: 30000, errors: ["time"] }).then(col=> {
                                 if(isNaN(col.content)) return interaction.followUp({ content: "Please input a valid number." })
                                 if(col.content > docs.cash) return interaction.followUp({ content: "Your cash is leess than the amount you want to deposit."})
 
                                 docs.bank += amount
                                 docs.cash -= amount 
-                                await docs.save();
+                                docs.save();
                                 return interaction.followUp({ content: `${client.config.checked} Successfully deposited \`${amount.toLocaleString()}\` to your bank account`})
                             })
                         })
