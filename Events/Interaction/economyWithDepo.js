225b7c233c621deae9b3800593fc2cb5dfe34deb
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-04 22:09:05 +0700

    Commit: #331

--- Events/Interaction/economyWithDepo.js
+++ Events/Interaction/economyWithDepo.js
@@ -33,7 +33,7 @@ module.exports = {
                 break;
                 case "done" :
                     if(docs.cash < docs.widht) return interaction.followUp({ content: `${client.config.cancel} Your cash is less than the amount you want to deposit`})
-                    interaction.update({ content: `${client.config,checked} Successfully deposited \`${docs.withd}\` to your bank account.`})
+                    interaction.update({ content: `${client.config.checked} Successfully deposited \`${docs.withd}\` to your bank account.`})
                     docs.bank = docs.withd
                     docs.widht = 0
                     docs.save()
