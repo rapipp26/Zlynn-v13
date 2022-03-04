d1a32d9cbbb4cbc43862d740d2f221d305e288bf
NotFlynn <78672226+rapipp26@users.noreply.github.com>
2022-03-04 22:15:24 +0700

    Commit: #333

--- Events/Interaction/economyWithDepo.js
+++ Events/Interaction/economyWithDepo.js
@@ -19,23 +19,23 @@ module.exports = {
                 case "10" :
                     docs.withd += 10
                     docs.save()
-                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\`` })
+                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\``, ephemeral: true })
                 break;
                 case "50" :
                     docs.withd += 50
                     docs.save()
-                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\`` })
+                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\``, ephemeral: true })
                 break;
                 case "100" :
                     docs.withd += 100
                     docs.save()
-                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\`` })
+                    interaction.update({ content: `Please click the button below to increase the amount you want to deposit. Amount: \`${docs.withd}\``, ephemeral: true })
                 break;
                 case "done" :
-                    if(docs.cash < docs.widht) return interaction.followUp({ content: `${client.config.cancel} Your cash is less than the amount you want to deposit`})
-                    interaction.update({ content: `${client.config.checked} Successfully deposited \`${docs.withd}\` to your bank account.`})
-                    docs.bank += docs.withd
-                    docs.widht = 0
+                    if(docs.cash < docs.withd) return interaction.followUp({ content: `${client.config.cancel} Your cash is less than the amount you want to deposit`, ephemeral: true })
+                    interaction.update({ content: `${client.config.checked} Successfully deposited \`${docs.withd}\` to your bank account.`, ephemeral: true })
+                    docs.bank += docs.withd;
+                    docs.withd = 0;
                     docs.save()
                 break;
             }
